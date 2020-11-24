8ff88970c9f46faf4a51d306f74ec6afae69050b
Chris Webb <chris@arachsys.com>
2020-11-24 17:17:22 +0000

    Import initial version of the Yamaha Montage shell
    This is the original 2017 Yamaha Montage upgrade.sh shell but with a new
    Makefile which builds the filesystem image without privileges in userspace
    using mke2fs and debugfs from e2fsprogs.

---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+RELEASE_VERSION=999.999.999
+
+exec <>/dev/tty0 2>&1
+reset
+
+while [ ! -b /dev/mmcblk0 ]; do
+  sleep 1
+done
+
+if [ ! -d /mnt/root ]; then
+  mkdir -p /mnt/root
+  mount -t ext4 -o discard,noatime,ro /dev/mmcblk0p2 /mnt/root
+fi
+
+if [ ! -d /mnt/home ]; then
+  mkdir -p /mnt/home
+  mount -t ext4 -o discard,noatime,ro /dev/mmcblk0p3 /mnt/home
+fi
+
+umount -l /mnt/loop
+getty -l sh -n 115200 tty0
+
+mount -o remount,ro /mnt/root
+mount -o remount,ro /mnt/home
+mount -o remount,ro /mnt/usb
+reboot -f
